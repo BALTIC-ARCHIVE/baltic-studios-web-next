b1e4b7c382f08faf8dd549a6a6395b5122f1e393
dieserjohn <john@baltic-studios.de>
2024-04-05 16:21:34 +0200

    add dialog to team-card

--- components/team-card/component.tsx
+++ components/team-card/component.tsx
@@ -2,6 +2,20 @@
 import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
 import Image from "next/image";
 import { MouseEvent } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Badge } from "../ui/badge";
+import { Card, CardContent, CardDescription, CardHeader } from "../ui/card";
+import { renderTextToHtml } from "@/lib/utils";
 
 export default function TeamCard({
   userName,
@@ -30,20 +44,53 @@ export default function TeamCard({
   }
 
   return (
-    <div className="tc bg-[#1A1A1A] p-2 rounded-md mx-2 flex w-[200px]">
-      <div className="">
-        <Image
-          className="h-12 w-12 rounded"
-          width={80}
-          height={80}
-          alt="alt"
-          src={getImageUrl(userName)}
-        />
-      </div>
-      <div className="ml-4">
-        <p>{userName}</p>
-        <span className="text-xs text-[#B2B2B2]">{rankName}</span>
-      </div>
-    </div>
+    <Dialog>
+      <DialogTrigger className="tc bg-[#1A1A1A] p-2 rounded-md mx-2 flex w-[200px]">
+        <div className="">
+          <Image
+            className="h-12 w-12 rounded"
+            width={80}
+            height={80}
+            alt="alt"
+            src={getImageUrl(userName)}
+          />
+        </div>
+        <div className="ml-4">
+          <p>{userName}</p>
+          <span className="text-xs text-[#B2B2B2]">{rankName}</span>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="flex flex-row w-full items-center">
+          <div className="w-1/5">
+            <Avatar className="h-24 w-24 rounded">
+              <AvatarImage src={getImageUrl(userName)} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className=" -ml-4 w-4/5">
+            <DialogTitle>{userName}</DialogTitle>
+            <DialogDescription className="mt-3">
+              <Badge className="uppercase px-[12px] py-[4px]" variant={"black"}>
+                {rankName}
+              </Badge>
+            </DialogDescription>
+          </div>
+        </DialogHeader>
+        <Card className="mb-10">
+          <CardHeader>Über Mich</CardHeader>
+          <CardContent>
+            <CardDescription
+              dangerouslySetInnerHTML={renderTextToHtml(bio)}
+            ></CardDescription>
+          </CardContent>
+        </Card>
+        <div className="flex-start">
+          <span className="text-[12px] text-gray-400">
+            Mitglied seit dem {entryDate}
+          </span>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function renderTextToHtml(text: string) {
+  // This is ONLY safe because the output HTML
+  // is shown to the same user, and because you
+  // trust this Markdown parser to not have bugs.
+  const renderedHTML = text;
+  return { __html: renderedHTML };
+}
